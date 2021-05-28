3e2e06a507896acbd6301bf9ea98b3501b3e8beb
MD Sofie Uddin Sohel <engrsofie@gmail.com>
2021-05-28 21:20:47 +0600

    add

--- src/components/home/NavBar/NavBar.js
+++ src/components/home/NavBar/NavBar.js
@@ -5,7 +5,9 @@ const NavBar = () => {
         <div>
             <h1>this is navbar</h1>
             <h2>I'm Sohel</h2>       
-            <h2>This is our new project</h2>       
+            <h2>This is our new project</h2> 
+            <h2>This is our new project2</h2> 
+                  
         </div>
     );
 };
