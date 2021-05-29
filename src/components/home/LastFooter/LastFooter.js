d6c19b09e594d7cdb89223972e92fe2515ea9f63
MD Sofie Uddin Sohel <engrsofie@gmail.com>
2021-05-30 01:09:26 +0600

    footer add

---
+++ src/components/home/LastFooter/LastFooter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LastFooter = () => {
+    return (
+        <div className="row m-3 ">
+           <div className="container ">
+           <h6>Copyright © 2019 Gladiator. All Rights Reserved Developed by- Retina Soft</h6>
+           </div>
+        </div>
+    );
+};
+
+export default LastFooter;
--- src/components/mainComponent/MainComponent.js
+++ src/components/mainComponent/MainComponent.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Equipment from '../home/Equipment/Equipment';
 import Facialities from '../home/Facialities/Facialities';
+import Footer from '../home/Footer/Footer';
+import LastFooter from '../home/LastFooter/LastFooter';
 import NavBar from '../home/NavBar/NavBar';
 
 const MainComponent = () => {
@@ -9,6 +11,8 @@ const MainComponent = () => {
         <NavBar/>
         <Facialities/>
         <Equipment/>
+        <Footer/>
+        <LastFooter/>
         </div>
     );
 };
