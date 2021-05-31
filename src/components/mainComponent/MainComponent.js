5780aae91a82bc6c55be0a97f1b3d9f73b4179d6
swapno <swapno5896@yahoo.com>
2021-05-31 01:50:13 -0700

    d

--- src/components/mainComponent/MainComponent.js
+++ src/components/mainComponent/MainComponent.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import Equipment from '../home/Equipment/Equipment';
-import Facialities from '../home/Facialities/Facialities';
-import Footer from '../home/Footer/Footer';
-import LastFooter from '../home/LastFooter/LastFooter';
-import NavBar from '../home/NavBar/NavBar';
-
+import React from "react";
+import Equipment from "../home/Equipment/Equipment";
+import Facialities from "../home/Facialities/Facialities";
+import Footer from "../home/Footer/Footer";
+import LastFooter from "../home/LastFooter/LastFooter";
+import NavBar from "../home/navBar/NavBar";
+import Class from '../home/Class/Class'
 const MainComponent = () => {
-    return (
-        <div>
-        <NavBar/>
-        <Facialities/>
-        <Equipment/>
-        <Footer/>
-        <LastFooter/>
-        </div>
-    );
+  return (
+    <div>
+      <NavBar />
+      <Class></Class>
+      <Facialities />
+      <Equipment />
+      <Footer />
+      <LastFooter />
+    </div>
+  );
 };
 
 export default MainComponent;
