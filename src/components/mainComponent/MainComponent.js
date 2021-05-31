fa1aab27aeb8fd6270af60a150145035a6d56a3d
swapno <swapno5896@yahoo.com>
2021-05-30 20:01:46 -0700

    fixed conflict

--- src/components/mainComponent/MainComponent.js
+++ src/components/mainComponent/MainComponent.js
@@ -2,11 +2,12 @@ import React from "react";
 import Class from "../Home/Class/Class";
 import Facilities from '../Home/Facilities/Facilities'
 import Equipment from '../Home/Equipment/Equipment'
+import NavBar from "../Home/navBar/NavBar";
 const MainComponent = () => {
   return (
     <div>
 
-
+<NavBar></NavBar>
       <Class></Class>
       <Facilities />
       <Equipment />
