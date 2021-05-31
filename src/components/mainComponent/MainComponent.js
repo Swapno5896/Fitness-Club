d0aef92b85ae2d55c6f2f63238547bcb90e4cd22
swapno <swapno5896@yahoo.com>
2021-05-30 21:15:53 -0700

    d

--- src/components/mainComponent/MainComponent.js
+++ src/components/mainComponent/MainComponent.js
@@ -1,16 +1,15 @@
 import React from "react";
-import Class from "../Home/Class/Class";
-import Facilities from '../Home/Facilities/Facilities'
-import Equipment from '../Home/Equipment/Equipment'
-import NavBar from "../Home/navBar/NavBar";
+import Facilities from "../home/Facilities/Facilities";
+import Class from "../home/Class/Class";
+import Equipment from '../home/Equipment/Equipment'
+import NavBar from "../home/navBar/NavBar";
 const MainComponent = () => {
   return (
     <div>
-
-<NavBar></NavBar>
-      <Class></Class>
+      <NavBar></NavBar>
+      <Class />
       <Facilities />
-      <Equipment />
+    <Equipment></Equipment>
     </div>
   );
 };
